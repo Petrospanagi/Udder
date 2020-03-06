85acf3c13c3092ff3f8d7774634e1bfbef29b615
Petros Panagi <petros_panagi@me.com>
2020-03-06 17:57:01 +0200

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Petros Panagi on 06/03/2020.
+//  Copyright © 2020 Petros Panagi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
